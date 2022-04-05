c014078bdd65562d466524adb4723b06451066e6
daksh-innovaccer <daksh.bindal@innovaccer.com>
2022-04-05 09:16:33 +0530

    5april_1

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import userstore from './Redux/redux-file';
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={userstore}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
